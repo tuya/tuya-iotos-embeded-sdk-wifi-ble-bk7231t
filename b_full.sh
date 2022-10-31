8d1109533a4032f6dfa2c3f6647301625c262586
valeklubomir <valeklubomir@gmail.com>
2022-10-31 23:51:50 +0100

    Tools update

--- b_full.sh
+++ b_full.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 ./build_app.sh apps/OpenBK7231T_App OpenBK7231T_App 1.0.0 clean
 ./build_app.sh apps/OpenBK7231T_App OpenBK7231T_App 1.0.0
+cp -f apps/OpenBK7231T_App/output/1.0.0/OpenBK7231T_App_1.0.0.rbl /var/www/OpenBK7231T_App_1.0.0.rbl
